6a5d7cbf4d992066ac3718755d0686a1221048c4
CloverSS <licrsy@163.com>
2016-09-22 15:18:03 +0800

    new

---
+++ DayGram/app/src/main/java/com/example/activitytest/daygram/Diary.java
@@ -0,0 +1,8 @@
+package com.example.activitytest.daygram;
+
+/**
+ * Created by apple on 2016/9/21.
+ */
+public class Diary {
+
+}
---
+++ DayGram/app/src/main/java/com/example/activitytest/daygram/editactivity.java
@@ -0,0 +1,7 @@
+package com.example.activitytest.daygram;
+
+/**
+ * Created by apple on 2016/9/21.
+ */
+public class editactivity {
+}
